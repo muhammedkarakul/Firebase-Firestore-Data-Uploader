9d63d0e7de6953e669adffbf4e2a3c2b471cbdb3
karakulmuhammed <muhammmed.karakul@loodos.com>
2020-03-09 15:03:45 +0300

    Initial Commit

---
+++ Remote Localization Helper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Remote Localization Helper
+//
+//  Created by Muhammed KARAKUL on 9.03.2020.
+//  Copyright © 2020 Loodos. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
